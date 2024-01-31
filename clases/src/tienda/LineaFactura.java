9b48ba8d0e949d753aa4e68d58bda978ee2155e8
“CesarMaarquez” <cesarmola4@gmail.com>
2024-01-31 10:05:59 +0100

    .

--- clases/src/tienda/LineaFactura.java
+++ clases/src/tienda/LineaFactura.java
@@ -2,9 +2,52 @@ package tienda;
 
 public class LineaFactura {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int codigo;
+	private float precio;
+	private int cantidad;
+	
+	public LineaFactura(int codigo, float precio, int cantidad) {
+		super();
+		this.codigo = codigo;
+		this.precio = precio;
+		this.cantidad = cantidad;
 	}
+
+	public LineaFactura(int codigo, float precio) {
+		super();
+		this.codigo = codigo;
+		this.precio = precio;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	@Override
+	public String toString() {
+		return "LineaFactura [codigo=" + codigo + ", precio=" + precio + ", cantidad=" + cantidad + "]";
+	}
+	
+	
 
 }
